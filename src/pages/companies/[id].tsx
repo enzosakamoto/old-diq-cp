9cc41729220168bcbdfe3633ef57cef04d446408
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-05-02 16:01:57 -0300

    fixed some things 😁

--- src/pages/companies/[id].tsx
+++ src/pages/companies/[id].tsx
@@ -3,41 +3,9 @@ import styles from "@/styles/Company.module.css";
 import { companies } from "@/data/companies/companies";
 import Image from "next/image";
 import Link from "next/link";
-import { CompanyEntity } from "@/shared/domain/entities/CompanyEntity";
 import Footer from "@/components/footer";
 import PageName from "@/components/head";
-import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-
-// interface Props {
-//   company: CompanyEntity;
-// }
-
-// export const getStaticPaths = async () => {
-//   const res = await fetch("@/data/companies.json/");
-//   const data = await res.json();
-
-//   const paths = data.map((company: any) => {
-//     return {
-//       params: { id: company.id.toString() },
-//     };
-//   });
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
-
-// export const getStaticProps = async (context: any) => {
-//   const id = context.params.id;
-//   const res = await fetch("@/data/companies.json/" + id);
-//   const data = res.json();
-
-//   return {
-//     props: { company: data },
-//   };
-// };
 
 export default function Company() {
   const router = useRouter();
