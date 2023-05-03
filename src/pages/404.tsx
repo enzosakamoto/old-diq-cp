29d38f87247f71221ee3041d5b6fc81ea1867cfd
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-05-03 15:27:47 -0300

    added 404 page 👌

---
+++ src/pages/404.tsx
@@ -0,0 +1,20 @@
+import Footer from "@/components/footer";
+import PageName from "@/components/head";
+import Navbar from "@/components/navbar";
+import styles from "@/styles/FourOhFour.module.css";
+import Image from "next/image";
+import fourohfour from "@/assets/fourohfour.svg";
+
+export default function FourOhFour() {
+  return (
+    <>
+      <PageName title="DIQ-CP | 404" content="Página não encontrada" />
+      <Navbar />
+      <main className={styles.page}>
+        <h1>Página não encontrada!</h1>
+        <Image src={fourohfour} width={400} height={400} alt="Erro 404" />
+      </main>
+      <Footer />
+    </>
+  );
+}
