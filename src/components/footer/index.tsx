5a8e54a8746d535d2a1979f864b824dac42625bc
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-05-11 15:04:11 -0300

    fixed colors 😎

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.component}>
-        <p>DIQ-CP IMT 2023</p>
+        <p>DIQ-CP IMT</p>
         <p>Esse site está em constante desenvolvimento | versão maio 2023</p>
       </footer>
     </>
