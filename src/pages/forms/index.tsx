73f0c93f9622c00a82245084a40ac75086ff0793
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-05-30 14:08:39 -0300

    fixed forms interfaces 😁

--- src/pages/forms/index.tsx
+++ src/pages/forms/index.tsx
@@ -7,7 +7,35 @@ import Link from 'next/link';
 import { ChangeEvent, useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Forms, Relations } from './interfaces';
+
+export interface Relations {
+  tcc: boolean;
+  icc: boolean;
+  ca: boolean;
+  pam: boolean;
+  pae: boolean;
+  smile: boolean;
+  pat: boolean;
+  hacka: boolean;
+  pe: boolean;
+  desc: boolean;
+  cons: boolean;
+  pa: boolean;
+  trei: boolean;
+  sol: boolean;
+  cal: boolean;
+  dev: boolean;
+  prot: boolean;
+}
+
+export interface Forms {
+  name: string;
+  company: string;
+  phone: string;
+  email: string;
+  relations: string[];
+  about: string;
+}
 
 export default function Forms() {
   const [forms, setForms] = useState<Forms>({
